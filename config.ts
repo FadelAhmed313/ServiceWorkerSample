09913cca95d1ca1e1cb8ec33f04b45e496528907
FadelAhmed313 <146953610+FadelAhmed313@users.noreply.github.com>
2024-04-08 13:55:51 +0400

    Add files via upload

---
+++ config.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyBp7MQkZHx00lpCthWsTd_0eO5Hc54bIso',
+    authDomain: 'chanda-calculator-c702c.firebaseapp.com',
+    projectId: 'chanda-calculator-c702c',
+    storageBucket: 'chanda-calculator-c702c.appspot.com',
+    messagingSenderId: '1086098697716',
+    appId: '1086098697716'
+};
+
+
+export const vapidKey = 'AAAA_OCG7fQ:APA91bEIiceVg1P9RoME0BWqmUPRnaTSsEZIDSd_463JC22mFNZT8QAGMsJkfmVyjnWvSzSx6PWYxS1NLUanR-zU00SoRVdnpcLBx-yUuJVXdZChz7MsGXaVwYlF3D6RFm2BLiyQeHKk';
